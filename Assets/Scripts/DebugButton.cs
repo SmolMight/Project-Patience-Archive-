ddd61a9fc36bfeeed48d962d66f37eef6b2c6198
Zan <NaGon.Dev@protonmail.com>
2021-05-22 02:36:38 +0800

    [*] Sample layout for GameScene
    [+] Resolution for Samsung A Series phones
    [+] GameScene to build settings
    [*] Set Unity Remote Device to "Any"

---
+++ Assets/Scripts/DebugButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugButton : MonoBehaviour
+{
+    public void PrintDebug()
+    {
+        Debug.Log("I've been clicked.");
+    }
+}
